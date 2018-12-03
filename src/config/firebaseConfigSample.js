ba5ee72042bd69acfe5426aa425030a3c909f6ce
alberto <albertodeagostini88@gmail.com>
2018-12-03 22:03:03 +0100

    fix config sample

--- src/config/firebaseConfigSample.js
+++ src/config/firebaseConfigSample.js
@@ -1,11 +1,10 @@
 // rename this file to firebaseConfig and insert your firebase data
 
-export default {  
+export default {
     apiKey: "your_apiKey",
     authDomain: "your_authDomain",
     databaseURL: "your_databaseURL",
     projectId: "your_projectId",
     storageBucket: "your_storageBucket",
-    messagingSenderId: "your_messagingSenderId",
-    databaseURL: "your_databaseURL",
+    messagingSenderId: "your_messagingSenderId"
 };
