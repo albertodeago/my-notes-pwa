5a309cd7a9615ef01218291626d75edf22becf64
alberto <albertodeagostini88@gmail.com>
2018-12-03 22:01:57 +0100

    fix gitignore

--- src/config/firebaseConfig.js
+++
@@ -1,9 +0,0 @@
-export default {  
-    apiKey: "AIzaSyAcconXP9_1H38Q4X5okEIzPEt7q2QK0D8",
-    authDomain: "my-notes88.firebaseapp.com",
-    databaseURL: "https://my-notes88.firebaseio.com",
-    projectId: "my-notes88",
-    storageBucket: "",
-    messagingSenderId: "585594638044",
-    databaseURL: "https://my-notes88.firebaseio.com",
-};
